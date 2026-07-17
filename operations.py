5fd004063086c5f88db53c9c76d6a0578f81307e
Fathimath Sherin B T <sherinbt123@gmail.com>
2026-07-17 20:10:48 +0530

    Add basic arithmetic operations in operations.py

---
+++ operations.py
@@ -0,0 +1,9 @@
+x=int(input("enter a number: "))
+y=int(input("enter a number: "))
+
+print(x)
+print(y)
+print("addition: ",x+y)
+print("subtraction: ",x-y)
+print("multiplication: ",x*y)
+print("division: ",x/y)
